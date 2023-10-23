af080bc8f746fc66258c3de0a02ba903f7309c0a
Dreamy Undertaker <vadiimreg73@gmail.com>
2023-10-24 01:49:50 +0400

    Готов скелет

--- my_site/main/urls.py
+++ my_site/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'home'),
-    path('about', views.about, name = 'about')
+    path('registration', views.registration, name = 'registration'),
+    path('signin', views.signin, name= 'signin'),
+    path('userAccount', views.userAccount, name= 'userAccount')
 ]
--- my_site/main/views.py
+++ my_site/main/views.py
@@ -5,5 +5,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'main/home.html')
 
-def about(request):
-    return render(request, 'main/about.html')
+def registration(request):
+    return render(request, 'main/registration.html')
+
+def signin(request):
+    return render(request, 'main/signin.html')
+
+def userAccount(request):
+    return render(request, 'main/userAccount.html')
