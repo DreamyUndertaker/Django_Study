f4dc017632c7c069b59ba42a4614a972947e20b2
Dreamy_Undertaker <vadiimreg73@gmail.com>
2023-10-28 10:57:23 +0400

    New classes in models

--- my_site/db/views.py
+++ my_site/db/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
-from .models import Articles
+from .models import Articles, Persons, Deposites, ExchangeRates, Clients, DepositsRegistration
 from .forms import ArticleForm
 
 # Create your views here.
 def db_home(request):
     db = Articles.objects.all()
-    return render(request, 'db/db.html', {'db' : db})
+    db1 = Persons.objects.all()
+    db2 = Deposites.objects.all()
+    db3 = ExchangeRates.objects.all()
+    db4 = Clients.objects.all()
+    db5 = DepositsRegistration.objects.all()
+    return render(request, 'db/db.html', {'db' : db, 'Persosns': db1, 'Deposites' : db2, 'ExchangeRates': db3, 'Clients': db4, 'DepositsRegistration': db5})
 
 def create(request):
     error = ''
@@ -13,7 +18,7 @@ def create(request):
         form = ArticleForm(request.post)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('')
         else:
             error = "форма была не верной"
 
